44f56f509ed50c0cb29b6d75b3732092a2ac8494
yang789yu <2695926793@qq.com>
2024-09-04 23:36:04 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import 'reset-css'   // 放在最前，清除默认样式
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
